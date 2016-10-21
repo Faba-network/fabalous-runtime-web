177dd5a20f1be7ec09e60f699ed17c7ebc618036
Jörg Wasmeier <j.wasmeier@faba.de>
2016-10-21 07:53:52 +0200

    Fix FabaEvent

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,6 @@
+/// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/systemjs/index.d.ts" />
 /// <reference path="modules/react-dom/index.d.ts" />
 /// <reference path="modules/react-router/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
