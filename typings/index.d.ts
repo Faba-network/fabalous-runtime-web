31d46dd44ea389fa49708cef5a3e16326f846c8b
Jörg Wasmeier <j.wasmeier@faba.de>
2016-11-18 14:09:08 +0100

    Remove React-Router

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,4 @@
-/// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/systemjs/index.d.ts" />
 /// <reference path="modules/react-dom/index.d.ts" />
 /// <reference path="modules/react-router/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
