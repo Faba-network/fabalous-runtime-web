26856207c028f286fbd296dbe753ffe481967cbe
Jörg Wasmeier <j.wasmeier@faba.de>
2016-11-18 14:22:38 +0100

    Remove React-Router

--- src/FabaWebBaseComponent.tsx
+++ src/FabaWebBaseComponent.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import shallowCompare from "react-addons-shallow-compare";
 
-class FabaWebBaseComponent<TProps> extends React.Component<TProps, null>{
-    constructor(props){
+export default class FabaWebBaseComponent<TProps> extends React.Component<TProps, null>{
+    constructor(props:TProps){
         super(props);
     }
 
