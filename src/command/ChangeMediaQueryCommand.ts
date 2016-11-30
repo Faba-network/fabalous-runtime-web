be2e278749b56e5c0b9307af851f44f6a3bb40e9
Jörg Wasmeier <j.wasmeier@faba.de>
2016-11-30 18:57:28 +0100

    Update build failures

--- src/command/ChangeMediaQueryCommand.ts
+++ src/command/ChangeMediaQueryCommand.ts
@@ -2,9 +2,9 @@ import ChangeMediaQueryEvent from "../event/ChangeMediaQueryEvent";
 import FabaCommand from "@fabalous/core/FabaCommand";
 import FabaStore from "@fabalous/core/FabaStore";
 
-export default class ChangeMediaQueryCommand extends FabaCommand<FabaStore> {
+export default class ChangeMediaQueryCommand extends FabaCommand<FabaStore<any>> {
     execute(event: ChangeMediaQueryEvent) {
-        //this.store.set("layout.landscape", event.landscape);
-        //this.store.set("layout.mobile", event.mobile);
+        this.store.set("layout.landscape", event.landscape);
+        this.store.set("layout.mobile", event.mobile);
     }
 }
