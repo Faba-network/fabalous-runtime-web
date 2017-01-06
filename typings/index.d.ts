1acf3358266233454dc330c27d77d001fe9058c5
Jörg Wasmeier <j.wasmeier@faba.de>
2017-01-06 09:36:37 +0100

    Update deps

--- typings/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/systemjs/index.d.ts" />
-/// <reference path="modules/react-dom/index.d.ts" />
-/// <reference path="modules/react/index.d.ts" />
-/// <reference path="modules/history/index.d.ts" />
-
-declare var CLIENT;
