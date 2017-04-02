1a22a6ea08d4f3c8bc51bf261897f3ae6b3c7b37
Jörg Wasmeier <j.wasmeier@faba.de>
2017-04-02 23:13:41 +0200

    Add Doc

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,25 @@
+require('@fabalous/core/config/gulp/gulp.config');
+
+var typedoc = require("gulp-typedoc");
 var gulp = require('gulp');
-var clean = require('gulp-clean');
-var replace = require('gulp-replace');
+gulp.task("typedoc", function() {
+    return gulp
+        .src(["src/*.ts", "src/transport/*.ts", "src/store/*.ts", "src/event/*.ts"])
+        .pipe(typedoc({
+            // TypeScript options (see typescript docs)
+            module: "commonjs",
+            target: "es5",
+            includeDeclarations: true,
 
-gulp.task('replace', function() {
-    return gulp.src('lib/**')
-        .pipe(replace(/\.\/\.\.\/typings\/index.d.ts/g, './typings/index.d.ts'))
-        .pipe(replace(/\.\.\/typings\/index.d.ts/g, './typings/index.d.ts'))
-        .pipe(gulp.dest('lib/'));
-});
-gulp.task('clean', function() {
-    return gulp.src('lib/')
-        .pipe(clean());
-});
+            // Output options (see typedoc docs)
+            out: "./doc",
 
-gulp.task('copy_src_to_lib', function() {
-    return gulp.src('lib/src/**')
-        .pipe(gulp.dest('lib/'));
-});
+            //theme : "./docTemplate",
 
-gulp.task('remove_src_folder', function() {
-    return gulp.src('lib/src', {read: false})
-        .pipe(clean());
-});
-
-gulp.task('remove_node_modules_folder', function() {
-    return gulp.src('lib/node_modules', {read: false})
-        .pipe(clean());
-});
-
-gulp.task('copy_config_folder', function() {
-    return gulp.src('config/**')
-        .pipe(gulp.dest('lib/config/'));
-});
-
-gulp.task('copy_package_json', function() {
-    return gulp.src('package.json')
-        .pipe(gulp.dest('lib/'));
+            // TypeDoc options (see typedoc docs)
+            name: "Fabalous Runtime Web",
+            ignoreCompilerErrors: true,
+            version: true,
+            excludeExternals:true
+        }));
 });
