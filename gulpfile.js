2dd9311929f836fc1dba89d651163f237d8e4586
Jörg Wasmeier <j.wasmeier@faba.de>
2017-04-03 17:58:43 +0200

    Fix Build issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,35 @@
-require('@fabalous/core/config/gulp/gulp.config');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+gulp.task('clean', function() {
+    return gulp.src('lib/')
+        .pipe(clean());
+});
+
+gulp.task('copy_src_to_lib', function() {
+    return gulp.src('lib/src/**')
+        .pipe(gulp.dest('lib/'));
+});
+
+gulp.task('remove_src_folder', function() {
+    return gulp.src('lib/src', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('remove_node_modules_folder', function() {
+    return gulp.src('lib/node_modules', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('copy_config_folder', function() {
+    return gulp.src('config/**')
+        .pipe(gulp.dest('lib/config/'));
+});
+
+gulp.task('copy_package_json', function() {
+    return gulp.src('package.json')
+        .pipe(gulp.dest('lib/'));
+});
 
 var typedoc = require("gulp-typedoc");
 var gulp = require('gulp');
