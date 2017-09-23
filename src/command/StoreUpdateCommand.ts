c597657db27b9d21f079d94e0f227676ae9d2cea
Jörg Wasmeier <j.wasmeier@faba.de>
2017-09-23 13:44:09 +0200

    Storeupdate dispatch only if event is not null

--- src/command/StoreUpdateCommand.ts
+++ src/command/StoreUpdateCommand.ts
@@ -5,7 +5,9 @@ import {FabaWebCommand} from "../FabaWebCommand";
 
 export default class StoreUpdateCommand extends FabaWebCommand<any> {
     async execute(event: FabaStoreUpdateEvent) {
-        let k = await FabaRuntimeWeb.activeEvent.dispatch();
-        new RenderToDOMEvent(FabaRuntimeWeb.rootComponent, "container", k.view).dispatch();
+        if (FabaRuntimeWeb.activeEvent){
+            let k = await FabaRuntimeWeb.activeEvent.dispatch();
+            new RenderToDOMEvent(FabaRuntimeWeb.rootComponent, "container", k.view).dispatch();
+        }
     }
 }
