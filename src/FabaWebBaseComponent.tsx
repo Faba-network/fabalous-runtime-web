3c997a24df556a4429a463f0d391a056e2392145
Jörg Wasmeier <j.wasmeier@faba.de>
2017-10-03 08:53:34 +0200

    Deps Update

--- src/FabaWebBaseComponent.tsx
+++ src/FabaWebBaseComponent.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-const shallowCompare = require('react-addons-shallow-compare');
-
 /**
  * Faba BaseComponent that use shallowCompare for quick rendering if it needs to
  */
@@ -12,16 +10,5 @@ export default class FabaWebBaseComponent<TProps, TState = {}> extends React.Pur
      */
     constructor(props:TProps){
         super(props);
-    }
-
-
-    /**
-     * Implemented by React
-     * @param nextProps
-     * @param nextState
-     * @returns {any}
-     */
-    shouldComponentUpdate(nextProps, nextState) {
-        return shallowCompare(this, nextProps, nextState);
     }
 }
