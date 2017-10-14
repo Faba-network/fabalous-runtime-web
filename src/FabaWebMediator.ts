c5d79cd32abda958693935ab09414c72f31f2ab5
Jörg Wasmeier <j.wasmeier@faba.de>
2017-10-14 15:19:27 +0200

    Update deps

--- src/FabaWebMediator.ts
+++ src/FabaWebMediator.ts
@@ -1,6 +1,4 @@
 import FabaCoreMediator from "@fabalous/core/FabaCoreMediator";
-import FabaEvent from "@fabalous/core/FabaEvent";
-import {FabaWebCommand} from "./FabaWebCommand";
 /**
  * Created by creativecode on 02.01.17.
  */
@@ -12,8 +10,6 @@ export class FabaWebMediator extends FabaCoreMediator{
      * @param check
      */
     addCommand(event, command, check?): void {
-        if (check && !check()) return;
-
-        super.addCommand(event, command);
+        super.addCommand(event, command, check);
     }
 }
